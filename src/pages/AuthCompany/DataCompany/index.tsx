e3c656d2517408995f818c692929b2831ea4f611
weslei <wesleirocha13@gmail.com>
2020-08-25 20:56:47 -0300

    Configurada a tela de "Meus dados" e "Editar Cadastro"

--- src/pages/AuthCompany/DataCompany/index.tsx
+++ src/pages/AuthCompany/DataCompany/index.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, ImageBackground, Text, KeyboardAvoidingView, Image } from 'react-native';
 import styles from './styles';
 import { RectButton, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
-import { useNavigation, Link, DrawerActions } from '@react-navigation/native';
+import { useNavigation, Link, DrawerActions, useFocusEffect } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
+import api from '../../../services/api';
+import AsyncStorage from '@react-native-community/async-storage';
 
 interface DataCompanyProps {
 
 };
 
 const DataCompany: React.FC<DataCompanyProps> = ({ children }) => {
+
+    const [name, setName] = useState('');
+    const [cnpj, setcnpj] = useState('');
+    const [email, setEmail] = useState('');
+    const [description, setDescription] = useState('');
+
+    async function loadDataCompany() {
+        const token = await AsyncStorage.getItem('CompanyToken');
+        const response = await api.get('companies/id', {
+            params: {
+                token: token,
+            }
+        });
+        setName(response.data.name);
+        setcnpj(response.data.cnpj);
+        setEmail(response.data.email);
+        setDescription(response.data.description);
+    };
+
+    useFocusEffect(
+        React.useCallback(() => {
+            loadDataCompany();
+        }, [])
+    );
+
     const { navigate } = useNavigation();
-    function handleNavigatetoEditDataCompany(){
+    function handleNavigatetoEditDataCompany() {
         navigate('EditDataCompany');
     };
     function handleNavigateToforgotPassword() { // serve para ir p/ próxima página, quando eu chamar essa função  vai direcionar para "GiveClasses"
@@ -22,23 +49,23 @@ const DataCompany: React.FC<DataCompanyProps> = ({ children }) => {
             {children}
             <View style={styles.elementsCenter}>
                 {/* Quando for usar uma imagem externa (Pegar por URL) tem que fazer desse jeito */}
-                <Image style={styles.avatar} source={{uri: 'https://github.com/wesleirocha13.png' }} />
-                <Text style={styles.nameCompany}>Empresa empresa empresa empresa empresa S.A</Text>
+                <Image style={styles.avatar} source={{ uri: 'https://github.com/wesleirocha13.png' }} />
+                <Text style={styles.nameCompany}> {name}</Text>
             </View>
             <View style={styles.elementsRight}>
                 <Text style={styles.textBold}>CNPJ:
-                    <Text style={styles.text}> 00.00.000/0000-00</Text> 
+                    <Text style={styles.text}> {cnpj}</Text>
                 </Text>
-                <Text style={styles.textBold}>E-mail: 
-                    <Text style={styles.text}> empresa@gmail.com</Text> 
+                <Text style={styles.textBold}>E-mail:
+                    <Text style={styles.text}> {email}</Text>
                 </Text>
                 <Text style={styles.textBold}>Descrição:
-                    <Text style={styles.text}> Essa é a descrição da empresa</Text> 
+                    <Text style={styles.text}> {description}</Text>
                 </Text>
                 <TouchableOpacity onPress={handleNavigateToforgotPassword} style={styles.forgotPassword}>
                     <Feather name="edit" size={20} color={'#EE0000'} />
                     <Text style={styles.underLineText}> Alterar senha</Text>
-                </TouchableOpacity>   
+                </TouchableOpacity>
             </View>
             <RectButton style={styles.okButton} >
                 <Text style={styles.okButtonText} onPress={handleNavigatetoEditDataCompany}>Editar Dados</Text>
--- src/pages/AuthCompany/EditDataCompany/index.tsx
+++ src/pages/AuthCompany/EditDataCompany/index.tsx
@@ -1,43 +1,101 @@
-import React from 'react';
-import { View, ImageBackground, Text, KeyboardAvoidingView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ImageBackground, Text, KeyboardAvoidingView, Alert } from 'react-native';
 import styles from './styles';
 import { RectButton, TextInput } from 'react-native-gesture-handler';
-import { useNavigation, Link } from '@react-navigation/native';
+import { useNavigation, Link, useFocusEffect } from '@react-navigation/native';
 import PageHeaderRegister from '../../../components/PageHeaderRegister';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
+import api from '../../../services/api';
+import AsyncStorage from '@react-native-community/async-storage';
 
 interface EditDataCompanyProps {
 
 };
 
 const EditDataCompany: React.FC<EditDataCompanyProps> = ({ children }) => {
+
+    const [name, setName] = useState('');
+    const [cnpj, setcnpj] = useState('');
+    const [email, setEmail] = useState('');
+    const [description, setDescription] = useState('');
+
+    async function loadDataCompany() {
+        const token = await AsyncStorage.getItem('CompanyToken');
+        const response = await api.get('companies/id', {
+            params: {
+                token: token,
+            }
+        });
+        setName(response.data.name);
+        setcnpj(response.data.cnpj);
+        setEmail(response.data.email);
+        setDescription(response.data.description);
+    };
+
+    useFocusEffect(
+        React.useCallback(() => {
+            loadDataCompany();
+        }, [])
+    );
+
     const { goBack } = useNavigation();
-    function handleGoBack(){
-        goBack();
+    async function saveEditions() {
+        try {
+            const token = await AsyncStorage.getItem('CompanyToken');
+            const response = await api.put('companies/', {
+                token: token,
+                name: name,
+                cnpj: cnpj,
+                email: email,
+                description: description
+            })
+
+            Alert.alert("Alteração realizada com sucesso!");
+            goBack();
+        } catch{
+            Alert.alert("Ocorreu um erro, tente novamente");
+        }
+
     };
     return (
         <View style={styles.container}>
-            <PageHeaderRegister title="Edição Cadastro"/>
+            <PageHeaderRegister title="Edição Cadastro" />
             <KeyboardAwareScrollView behavior="position" enabled>
                 <View style={styles.containerForm}>
                     <View style={styles.inputPadding}>
                         <Text style={styles.label}>Nome</Text>
-                        <TextInput style={styles.input} placeholder="Nome" />
+                        <TextInput style={styles.input}
+                            placeholder="Nome"
+                            value={name}
+                            onChangeText={setName}
+                        />
                     </View>
                     <View style={styles.inputPadding}>
                         <Text style={styles.label} >CNPJ</Text>
-                        <TextInput style={styles.input} placeholder="CNPJ" />
+                        <TextInput style={styles.input}
+                            placeholder="CNPJ"
+                            value={cnpj}
+                            onChangeText={setcnpj}
+                        />
                     </View>
                     <View style={styles.inputPadding}>
                         <Text style={styles.label}>E-mail</Text>
-                        <TextInput style={styles.input} placeholder="E-mail" />
+                        <TextInput style={styles.input}
+                            placeholder="E-mail"
+                            value={email}
+                            onChangeText={setEmail}
+                        />
                     </View>
                     <View style={styles.inputPadding}>
                         <Text style={styles.label}>Descrição</Text>
-                        <TextInput style={styles.input} placeholder="Descrição" />
+                        <TextInput style={styles.input}
+                            placeholder="Descrição"
+                            value={description}
+                            onChangeText={setDescription}
+                        />
                     </View>
-                    <RectButton style={styles.okButton} >
-                        <Text style={styles.okButtonText} onPress={handleGoBack}>Salvar</Text>
+                    <RectButton style={styles.okButton} onPress={saveEditions}>
+                        <Text style={styles.okButtonText}>Salvar</Text>
                     </RectButton>
                 </View>
             </KeyboardAwareScrollView>
